5a59aff3bbd5662b88c8c62799db67a312a18bf7
maldrek3103 <cpisanu96@gmail.com>
2021-09-22 17:25:23 +0200

    Added more style + footer

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $questions = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css' integrity='sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==' crossorigin='anonymous'/>
     <title>Google FAQ</title>
 </head>
 <body>
@@ -56,14 +56,14 @@ $questions = [
 
         <!-- LOGO + TEXT -->
         <div class="me-auto">
-          <span>logo</span>
-          <span>privacy</span>
+          <span><img src="./img/Google-Logo.png" alt="Google"></span>
+          <span>Privacy e Termini</span>
         </div>
 
         <!-- MENU + USER -->
         <div>  
-          <span  class="px-3">menu</span>
-          <span >user</span> 
+          <span  class="px-3"><i class="fas fa-th"></i></span>
+          <span ><i class="fas fa-user-circle"></i></span> 
         </div>
       </div>
 
@@ -77,8 +77,10 @@ $questions = [
           <a class="li-style" href="#">Domande frequenti</a>    
         </div>
       </nav>
-            
+      
+
     </header>
+
 
     <!-- MAIN -->
     <main>
@@ -99,16 +101,58 @@ $questions = [
         
     </main>
 
+    <!-- FOOTER -->
+    <footer class="py-3">
 
+    <div class="container d-flex">
+      <div class="me-auto">
+        <span>Google -</span>
+        <span>Tutto su Google -</span>
+        <span>Privacy -</span>
+        <span>Termini</span>
+      </div>
+
+      
+      <div>  
+        <span><i class="fas fa-globe-europe"></i></span>
+        <span>
+          <select id="lang">
+            <option value="it-IT">Italiano</option>
+            <option value="en-EN">Inglese</option>
+            <option value="es-ES">Spagnolo</option>
+            <option value="fr-FR">Francese</option>
+          </select>
+        </span>
+      </div>
+    </div>        
+
+    </footer>
 
 </body>
 </html>
 
 <style lang="css">
+
+/* Utilis */
 .li-style{
   list-style-type: none;
   text-decoration: none;
   padding-right: 2rem;
 }
-
+/* General */
+header{
+  position: fixed;
+  width: 100%;
+  border-bottom: 1px solid #ccc;
+  background-color: #fff;
+}
+header img{
+  height: 40px;
+}
+main{
+  padding-top: 200px;
+}
+footer{
+  background-color: #ccc;
+}
 </style>
